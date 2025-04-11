494bfa2c11e0d96ccb92e9253bd987863c94245d
erkeg <erkegalielbosynovic@gmail.com>
2025-04-11 08:58:30 +0500

    master

--- Assigment2/src/MyArrayList.java
+++ Assigment2/src/MyArrayList.java
@@ -1,19 +1,55 @@
+import java.util.Arrays;
 import java.util.Iterator;
 
+import static com.sun.tools.javac.util.ArrayUtils.ensureCapacity;
+
 public class MyArrayList<T> implements MyList<T>{
+    private T[] elements;
+    private int size;
+    private static final int DEFAULT_CAPACITY = 10;
+
+    public MyArrayList() {
+        this.elements = (T[]) new Object[DEFAULT_CAPACITY];
+        this.size = 0;
+    }
+
+    public MyArrayList(int initialCapacity) {
+        if(initialCapacity > 0){
+            this.elements = (T[]) new Object[initialCapacity];
+        } else if(initialCapacity == 0){
+            this.elements = (T[]) new Object[0];
+        } else{
+            throw new IllegalArgumentException("Illegal Capacity: " + initialCapacity);
+        }
+    }
+    
     @Override
     public void add(T item) {
+        ensureCapacity(size + 1);
+        elements[size++] = item;
+    }
 
+    private void ensureCapacity(int i) {
+        if(size == elements.length){
+            elements = Arrays.copyOf(elements, size + CAPACITY);
+        }
     }
 
     @Override
     public void set(int index, T item) {
-
+        checkIndex(index);
+        elements[index] = item;
     }
 
     @Override
     public void add(int index, T item) {
-
+        if (index < 0 || index > size) {
+            throw new IndexOutOfBoundsException("Index: " + index + ", Size: " + size);
+        }
+        ensureCapacity(size + 1);
+        System.arraycopy(elements, index, elements, index + 1, size - index);
+        elements[index] = item;
+        size++;
     }
 
     @Override
