8486397f362061d1d6f57b733dcad54d6d1108f4
erkeg <erkegalielbosynovic@gmail.com>
2025-04-27 23:07:28 +0500

    master

---
+++ Assigment3/src/MyTestingClass.java
@@ -0,0 +1,25 @@
+public class MyTestingClass {
+    private int id;
+
+    public MyTestingClass(int id) {
+        this.id = id;
+    }
+
+    @Override
+    public int hashCode() {
+        return id * 31;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        MyTestingClass that = (MyTestingClass) obj;
+        return id == that.id;
+    }
+
+    @Override
+    public String toString() {
+        return "MyTestingClass{" + "id=" + id + '}';
+    }
+}
